1483f0261b9e568e339c3fd621aa068796983f83
Martin Nohava <martin.nohava.mn@gmail.com>
2020-11-10 10:53:12 +0100

    Test build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,61 @@
 <template>
-    <v-layout row justify-space-between>
-        <v-flex lg3 class="mx-3 mt-7 d-none d-lg-block">
-          <v-sheet
-              rounded="lg"
-              :elevation="6"    
-            >         
-                <!-- LEFT ON lg-->
-              <WellcomePanel></WellcomePanel>
-            </v-sheet>
-        </v-flex>
-        <v-flex md12 class="mx-3 mt-7 d-lg-none">
-          <v-sheet
-              rounded="lg"
-              :elevation="6"    
-            >         
-                <!-- LEFT ON sm, md -->
-              <WellcomePanel></WellcomePanel>
-            </v-sheet>
-        </v-flex>
-        <v-flex sm12 md7 lg6 class="mx-3 mt-7">
-          <v-sheet
-              rounded="lg"
-              min-height="420"
-              :elevation="6"
-            >
-               <!-- MAIN -->
-               <MainPanel></MainPanel>
-            </v-sheet>
-        </v-flex>
-        <v-flex sm12 md4 lg2 class="mx-3 mt-7">
-          <v-layout column>
-            <v-flex>
-              <v-sheet
-              rounded="lg"
-              :elevation="6"
-            >
-              <!-- RIGHT UP -->
-              <CustomizationPanel></CustomizationPanel>
-            </v-sheet>
-            </v-flex>
-            <v-flex class="mt-7">
-              <v-sheet
-              rounded="lg"
-              min-height="200"
-              :elevation="6"
-            >
-              <!-- RIGHT DOWN -->
-              <HelpPanel></HelpPanel>
-            </v-sheet>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
+    <v-container>
+        <v-row>
+            <v-col lg="3" class="mx-3 mt-7 d-none d-lg-block">
+            <v-sheet
+                rounded="lg"
+                :elevation="6"    
+                >         
+                    <!-- LEFT ON lg-->
+                <WellcomePanel></WellcomePanel>
+                </v-sheet>
+            </v-col>
+            <v-col md="12" class="mx-3 mt-7 d-lg-none">
+            <v-sheet
+                rounded="lg"
+                :elevation="6"    
+                >         
+                    <!-- LEFT ON sm, md -->
+                <WellcomePanel></WellcomePanel>
+                </v-sheet>
+            </v-col>
+            <v-col sm="12" md="7" lg="6" class="mx-3 mt-7">
+            <v-sheet
+                rounded="lg"
+                min-height="420"
+                :elevation="6"
+                >
+                <!-- MAIN -->
+                <MainPanel></MainPanel>
+                </v-sheet>
+            </v-col>
+            <v-col sm="12" md="4" lg="2" class="mx-3 mt-7">
+                <v-row>
+                    <v-col>
+                        <v-sheet
+                        rounded="lg"
+                        :elevation="6"
+                        >
+                        <!-- RIGHT UP -->
+                        <CustomizationPanel></CustomizationPanel>
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col class="mt-7">
+                        <v-sheet
+                        rounded="lg"
+                        min-height="200"
+                        :elevation="6"
+                        >
+                        <!-- RIGHT DOWN -->
+                        <HelpPanel></HelpPanel>
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
